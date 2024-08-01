325dd0ee3f92698b45996ed56866e5d4d3511a3d
jhaashutosh <jhaashutosh58@gmail.com>
2024-08-02 00:20:46 +0530

    made some changes for proxy and also made some changes to vercel.json file

--- frontend/src/components/pages/NotesPage.js
+++ frontend/src/components/pages/NotesPage.js
@@ -4,7 +4,7 @@ import Note from '../Note';
 import CreateGroupModal from '../CreateGroupModal';
 import NoteInput from '../NoteInput';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://note-making-app-api.vercel.app/';
 
 const NotesPage = () => {
   const [groups, setGroups] = useState([]);
